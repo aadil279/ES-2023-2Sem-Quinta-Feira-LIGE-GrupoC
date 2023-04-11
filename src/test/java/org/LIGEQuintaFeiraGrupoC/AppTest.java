396059e2e200f93917ddae38a0dd8aef521b4259
aadilPort <aadilneedsmoreimagination@gmail.com>
2023-04-11 11:24:40 +0100

    Initial Setup

---
+++ src/test/java/org/LIGEQuintaFeiraGrupoC/AppTest.java
@@ -0,0 +1,10 @@
+package org.LIGEQuintaFeiraGrupoC;
+
+import org.LIGEQuintaFeiraGrupoC.App;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+}
