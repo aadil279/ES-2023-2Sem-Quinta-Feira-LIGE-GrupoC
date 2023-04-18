6731c1f5b78b811e965b8f5b839e15307c08b8c1
Luis <larcs@iscte-iul.pt>
2023-04-18 22:34:29 +0100

    Updated class ReadFile[private List getDataCSV(File file)]. Unitary tests are needed.

--- src/main/java/org/LIGEQuintaFeiraGrupoC/ReadFile.java
+++ src/main/java/org/LIGEQuintaFeiraGrupoC/ReadFile.java
@@ -8,7 +8,11 @@ import java.nio.file.Path;
 
 import java.util.Arrays;
 import java.util.List;
+
+import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+
 import java.util.Map;
 
 public class ReadFile {
@@ -34,15 +38,23 @@ public class ReadFile {
         return null;
     }
 
-    private List getDataCSV(File file) {
-        return null;
-    }
+    /**
+     * Transforms the content of a file into a Map
+     * @param file should be formatted as a csv file
+     * @return returns a type List<Map> where every Map is a row and contains the values of every column
+     * @throws IOException
+     */
+    private List getDataCSV(File file) throws IOException {
 
+        MappingIterator<Map> personIter = new CsvMapper().readerWithTypedSchemaFor(Map.class).readValues(file);
+        List<Map> list = personIter.readAll();
+        return list;
+    }
 
     /**
      * Transforms the content of a file into a Map
-     * @param file this file should be formatted as a json list
-     * @return returns a type List<Map> where every Map is contains the attributes of the json element
+     * @param file should be formatted as a json list
+     * @return returns a type List<Map> where every Map contains the attributes of the json element
      * @throws IOException
      */
     private List getDataJSON(File file) throws IOException {
