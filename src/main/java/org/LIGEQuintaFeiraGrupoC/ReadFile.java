606ce4a4483206f483b1105b04c9cedb17b368ad
Luis <larcs@iscte-iul.pt>
2023-04-18 19:10:00 +0100

    Updated class ReadFile[private List getDataJson(File file)]. Unitary tests are needed.

--- src/main/java/org/LIGEQuintaFeiraGrupoC/ReadFile.java
+++ src/main/java/org/LIGEQuintaFeiraGrupoC/ReadFile.java
@@ -1,7 +1,15 @@
 package org.LIGEQuintaFeiraGrupoC;
 
 import java.io.File;
+import java.io.*;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import java.util.Arrays;
 import java.util.List;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Map;
 
 public class ReadFile {
     public File getFile(String file) {
@@ -30,9 +38,22 @@ public class ReadFile {
         return null;
     }
 
-    private List getDataJSON(File file) {
-        //Use dependency (maybe jackson)
-        return null;
+
+    /**
+     * Transforms the content of a file into a Map
+     * @param file this file should be formatted as a json list
+     * @return returns a type List<Map> where every Map is contains the attributes of the json element
+     * @throws IOException
+     */
+    private List getDataJSON(File file) throws IOException {
+
+        Path path = Path.of(file.getPath());
+        String jsonList = Files.readString(path);
+
+        ObjectMapper mapper = new ObjectMapper();
+        List<Map> list = Arrays.asList(mapper.readValue(jsonList, Map[].class));
+
+        return list;
     }
 
 }
--- src/test/java/org/LIGEQuintaFeiraGrupoC/ReadFileTest.java
+++ src/test/java/org/LIGEQuintaFeiraGrupoC/ReadFileTest.java
@@ -1,4 +1,22 @@
 package org.LIGEQuintaFeiraGrupoC;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class ReadFileTest {
+    @Test
+    void getFile() {
+    }
+
+    @Test
+    void isValidFile() {
+    }
+
+    @Test
+    void getData() {
+    }
+
+    @Test
+    void testGetFile() {
+    }
 }
