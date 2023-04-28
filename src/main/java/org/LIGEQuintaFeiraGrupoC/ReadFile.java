7c4177be861c19dd39fae952cb877036e75ce876
Luis <larcs@iscte-iul.pt>
2023-04-28 13:03:53 +0100

    Read from URI : DONE

--- src/main/java/org/LIGEQuintaFeiraGrupoC/ReadFile.java
+++ src/main/java/org/LIGEQuintaFeiraGrupoC/ReadFile.java
@@ -6,19 +6,33 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+
+import jfxtras.icalendarfx.VCalendar;
+import jfxtras.icalendarfx.components.VEvent;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 import java.util.Map;
 
 public class ReadFile {
+    public static final String URI_HEAD = "webcal";
+    public static final String HTTP_HEAD = "http";
+
     public File getFile(String file) {
         // If file is local, readLocalFile(file), if is online, readOnlineFile().
-        return null;
+        return readOnlineFile(file);
     }
 
     private File readLocalFile(String file) {
@@ -26,6 +40,25 @@ public class ReadFile {
     }
 
     private File readOnlineFile(String file) {
+        if(file.contains(URI_HEAD)) {
+            int n = URI_HEAD.length();
+            file = HTTP_HEAD + file.substring(n);
+        }
+
+
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder().url(file).build();
+
+        try {
+            Response response = client.newCall(request).execute();
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+            return new File(response.body().string());
+
+        } catch (IOException e) {
+            System.err.println("Couldn't transform the file");
+        }
+
         return null;
     }
 
@@ -34,8 +67,15 @@ public class ReadFile {
     }
 
     public List getData(File file) {
-        //if file is csv, getDataCSV. if json, getDataJson)
-        return null;
+        List result = new ArrayList<>();
+
+        try {
+            result.addAll(getDataICS(file));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return result;
     }
 
     /**
@@ -68,4 +108,10 @@ public class ReadFile {
         return list;
     }
 
+    private List getDataICS(File file) throws IOException {
+        VCalendar calendar = VCalendar.parse(file.toString());
+        List<VEvent> events = calendar.getVEvents();
+
+        return events;
+    }
 }
--- src/test/java/org/LIGEQuintaFeiraGrupoC/ReadFileTest.java
+++ src/test/java/org/LIGEQuintaFeiraGrupoC/ReadFileTest.java
@@ -3,7 +3,14 @@ package org.LIGEQuintaFeiraGrupoC;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Scanner;
+
 public class ReadFileTest {
+    final String horarioISCTE = "webcal://fenix.iscte-iul.pt/publico/publicPersonICalendar.do?method=iCalendar&username=larcs@iscte.pt&password=z7wVbBXBgvFsLak6Fniz4axxxOgdJAQ9lYpC50hHZNJW4peqyW7dn7OkMOzR8cdskCctRO4p2m3UZXBcI3iuHSLqLUAHOvVXJEeUY9ay7dxNXx3O6RRdKh0VsLKmrfoE";
+
     @Test
     void getFile() {
     }
@@ -18,5 +25,37 @@ public class ReadFileTest {
 
     @Test
     void testGetFile() {
+    }
+
+    @Test
+    void testGetOnlineFileISCTE() {
+        ReadFile read = new ReadFile();
+        File file = read.getFile(horarioISCTE);
+
+        assert(file != null);
+        assert(!file.toString().isEmpty());
+
+        System.out.println("Reading file\n" +file.toString());
+        System.out.println("---------READ----------");
+    }
+
+    @Test
+    void testDataOnlineFileISCTE() {
+        ReadFile read = new ReadFile();
+        File file = read.getFile(horarioISCTE);
+
+        assert(file != null);
+        assert(!file.toString().isEmpty());
+
+        List list = read.getData(file);
+
+        assert(list!=null);
+        System.out.println(list.isEmpty());
+
+        System.out.println("Reading file");
+        for(Object o : list) {
+            System.out.println(o.toString());
+        }
+        System.out.println("---------READ----------");
     }
 }
