153f63a926afe4db978c640934427889d5f55d95
Luis <larcs@iscte-iul.pt>
2023-04-29 16:07:44 +0100

    Updated classes Calendar a and tests, included static Agenda createAgendaFromList(List<Map<?,?>> list)

--- src/main/java/org/LIGEQuintaFeiraGrupoC/Calendar.java
+++ src/main/java/org/LIGEQuintaFeiraGrupoC/Calendar.java
@@ -4,16 +4,28 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.util.Callback;
+
 import jfxtras.scene.control.agenda.Agenda;
 import jfxtras.scene.control.agenda.AgendaSkinSwitcher;
 import jfxtras.scene.layout.VBox;
 
+import java.io.File;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
 
 
 public class Calendar extends Application {
+
+    private List<Map<?,?>> events = new ArrayList<>();
+
+    public Calendar(List<Map<?,?>> events) {
+            this.events.addAll(events);
+    }
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -26,9 +38,37 @@ public class Calendar extends Application {
                         .withDescription("ES")
                         .withSummary("Engenharia de Software")
         );
-        AgendaSkinSwitcher ss = new AgendaSkinSwitcher(agenda);
+
+        AgendaSkinSwitcher ss = new AgendaSkinSwitcher(createAgendaFromList(events));
+        //AgendaSkinSwitcher ss = new AgendaSkinSwitcher(agenda);
         VBox root = new VBox(agenda, ss);
         stage.setScene(new Scene(root));
         stage.show();
+    }
+
+    public static Agenda createAgendaFromList(List<Map<?,?>> list) {
+        Agenda agenda = new Agenda();
+
+        for(Map<?,?> m : list) {
+            CharSequence inicDateChar = (CharSequence)(m.get("Data da aula").toString() + " "+ m.get("Hora início da aula").toString());
+            LocalDateTime inicDate = LocalDateTime.parse(inicDateChar, DateTimeFormatter.ofPattern("dd/mm/yyyy hh:mm:ss"));
+
+            CharSequence endTimeChar = (CharSequence)(m.get("Hora fim da aula").toString());
+            LocalDateTime endTime = LocalDateTime.parse(endTimeChar, DateTimeFormatter.ofPattern("hh:mm:ss"));
+            LocalDateTime endDate = inicDate.plusHours(endTime.getHour()).plusMinutes(endTime.getMinute()).plusSeconds(endTime.getSecond());
+
+            String description = m.toString();
+            String summary = m.get("Unidade Curricular").toString();
+
+            agenda.appointments().add(
+                    new Agenda.AppointmentImplLocal()
+                            .withStartLocalDateTime(inicDate)
+                            .withEndLocalDateTime(endDate)
+                            .withDescription(description)
+                            .withSummary(summary)
+            );
+        }
+
+        return agenda;
     }
 }
---
+++ src/test/java/org/LIGEQuintaFeiraGrupoC/CalendarTest.java
@@ -0,0 +1,14 @@
+package org.LIGEQuintaFeiraGrupoC;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalendarTest {
+
+    @Test
+    void createAgendaFromList() {
+    }
+}
